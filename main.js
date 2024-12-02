1180c710470ef86a0d8db781ed35cd1fc14d247b
Avaz <avazbekrozimboyev08@gmail.com>
2024-12-02 14:34:29 +0500

    geermec

---
+++ main.js
@@ -0,0 +1,43 @@
+// let son="  Find spaces here  ";
+// let res=son.trim();
+// let resol=[];
+
+//21-masa
+// for(let i = 0; i < res.length; i++){
+//     if(res[i] === " "){
+//         resol.push(i)
+//     }
+// }
+
+// console.log(resol)
+
+//2-masala
+
+// let soz = "olma nok olma nok anor nok";
+// let a=soz.split(' ');
+// let res = [];
+// for(let i = 0; i < a.length; i++){
+// if(!res.includes(a[i])){
+//     res.push(a[i])
+// }
+// }
+// console.log(res.join(","));
+
+//3-masala
+// let soz = "JavaScriptIsAmazing";
+// let b = soz.slice(4, 14).toLowerCase();
+
+// console.log(b);
+
+//4-masal
+
+// let soz="ascbka7686gdasd85dadg";
+// let newSoz = ""
+
+// for (let i = 0; i < soz.length; i++) {
+
+//     if (isNaN(+soz[i])) {
+//         newSoz+=soz[i]
+//     }
+// }
+// console.log(newSoz);
